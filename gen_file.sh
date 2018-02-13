a62e9c006bfa4e56148e88f27fc2c26fefa5ae55
jacob john day <jday@linux60414.dc.engr.scu.edu>
2018-02-13 11:09:46 -0800

    First commit

---
+++ gen_file.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#check arguments
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <SIZE> <BASE>" >&2
+  exit 1
+fi
+
+out="$1$2.dat"
+
+#test write speed, use sync to write data buffered in memory out to disk
+sync; dd if=/dev/urandom of=/local/weka/$out count=1024 iflag=count_bytes; sync
+
+#test read speed
+dd if=/local/weka/$out of=/dev/urandom count=1024 iflag=count_bytes
+
+cat /dev/urandom | head -c $1$2 > /local/weka/$out
+
+#display file size
+du -h /local/weka/$out
