b5e1cf1646d26656fccb1c18d526a7e56327e659
jacob john day <jday@linux60807.dc.engr.scu.edu>
2018-02-13 11:25:01 -0800

    Added option to clear cache

--- gen_file.sh
+++ gen_file.sh
@@ -11,6 +11,9 @@ out="$1$2.dat"
 #test write speed, use sync to write data buffered in memory out to disk
 sync; dd if=/dev/urandom of=/local/weka/$out count=1024 iflag=count_bytes; sync
 
+#clear cached data from RAM so it is only read from disk, uncomment if root user
+#sudo /sbin/sysctl -w vm.drop_caches=3
+
 #test read speed
 dd if=/local/weka/$out of=/dev/urandom count=1024 iflag=count_bytes
 
